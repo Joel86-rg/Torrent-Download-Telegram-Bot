dc971e89547ad6cb0f099c1e6103cdac60e74729
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2022-04-04 19:27:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def start_handler(_, message: Message):
         await message.reply_text(
             text="🤗Hello, I am simple Torrent Search Bot!\n"
                  "😎I can search Torrent Magnetic Links from Inline.\n\n"
-                 "✨Developer: Sanila Ranatunga\n✨Feedback : @sanilaassistant_bot",
+                 "✨Developer: Sanila Ranatunga\n✨Feedback : [Submit by clicking](https://t.me/sanilaassistant_bot)",
             disable_web_page_preview=True,
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(DEFAULT_SEARCH_MARKUP)
@@ -99,7 +99,7 @@ async def inline_handlers(_, inline: InlineQuery):
                                              f"**🔗Leechers:** `{torrentList[i]['Leechers']}`\n"
                                              f"**📤Uploader:** `{torrentList[i]['Uploader']}`\n"
                                              f"**📭Uploaded :** {torrentList[i]['Date']}**\n\n"
-                                             f"**🗒Magnet:**\n`{torrentList[i]['Magnet']}`\n\n Developer: Sanila Ranatunga\n Feedback : @sanilaassistant_bot ",
+                                             f"**🗒Magnet:**\n`{torrentList[i]['Magnet']}`\n\n Developer: Sanila Ranatunga\n Feedback : [Submit by clicking](https://t.me/sanilaassistant_bot)",
                                 parse_mode="Markdown"
                             ),
                             reply_markup=InlineKeyboardMarkup(
@@ -151,7 +151,7 @@ async def inline_handlers(_, inline: InlineQuery):
                                              f"**🌟Rating:** `{torrentList[i]['Rating']}`\n"
                                              f"**📊Duration:** `{torrentList[i]['Runtime']}`\n"
                                              f"**🗓Released on {torrentList[i]['ReleaseDate']}**\n\n"
-                                             f"**🔗Torrent Download Links:**\n{dl_links}\n\n  Developer: Sanila Ranatunga\n Feedback : @sanilaassistant_bot ",
+                                             f"**🔗Torrent Download Links:**\n{dl_links}\n\n  Developer: Sanila Ranatunga\n Feedback : [Submit by clicking](https://t.me/sanilaassistant_bot) ",
                                 parse_mode="Markdown",
                                 disable_web_page_preview=True
                             ),
@@ -202,7 +202,7 @@ async def inline_handlers(_, inline: InlineQuery):
                                              f"**🔗Leechers:** `{torrentList[i]['Leecher']}`\n"
                                              f"**📊Size:** `{torrentList[i]['Size']}`\n"
                                              f"**📭Uploaded on:** `{torrentList[i]['Date']}`\n\n"
-                                             f"**🗒Magnet:** \n`{torrentList[i]['Magnet']}`\n\n Developer: Sanila Ranatunga\n Feedback : @sanilaassistant_bot ",
+                                             f"**🗒Magnet:** \n`{torrentList[i]['Magnet']}`\n\n Developer: Sanila Ranatunga\n Feedback : [Submit by clicking](https://t.me/sanilaassistant_bot)",
                                 parse_mode="Markdown"
                             ),
                             reply_markup=InlineKeyboardMarkup(
@@ -242,7 +242,7 @@ async def inline_handlers(_, inline: InlineQuery):
                                          f"__Uploaded by {torrentList[i]['UploadedBy']}__\n"
                                          f"__Uploaded {torrentList[i]['DateUploaded']}__\n"
                                          f"__Last Checked {torrentList[i]['LastChecked']}__\n\n"
-                                         f"**Magnet:**\n`{torrentList[i]['Magnet']}`\n\n Developer: Sanila Ranatunga\n Feedback : @sanilaassistant_bot ",
+                                         f"**Magnet:**\n`{torrentList[i]['Magnet']}`\n\n Developer: Sanila Ranatunga\n Feedback : [Submit by clicking](https://t.me/sanilaassistant_bot) ",
                             parse_mode="Markdown"
                         ),
                         reply_markup=InlineKeyboardMarkup(
