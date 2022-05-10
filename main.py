d91a48428790c49b13dbc730befb9308893e29b9
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2022-05-10 22:32:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, 
 from configs import Config
 from tool import SearchYTS, SearchAnime, Search1337x, SearchPirateBay
 
-TorrentBot = Client(session_name=Config.SESSION_NAME, api_id=Config.API_ID, api_hash=Config.API_HASH, bot_token=Config.BOT_TOKEN)
+TorrentBot = Client(api_id=Config.API_ID, api_hash=Config.API_HASH, bot_token=Config.BOT_TOKEN)
 DEFAULT_SEARCH_MARKUP = [
                     [InlineKeyboardButton("🔎Search YTS", switch_inline_query_current_chat="!yts "),
                      InlineKeyboardButton("🔎Go Inline", switch_inline_query="!yts ")],
